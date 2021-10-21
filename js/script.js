0e35b592b6c189efa093b0b2d1422f83d4598f21
Nikita <khakimov.nikita.vch@gmail.com>
2021-10-21 21:12:51 +0300

    fix

---
+++ js/script.js
@@ -0,0 +1,85 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+	//Tabs
+	const tabsContent = document.querySelectorAll('.tabcontent');
+	const tabs = document.querySelectorAll('.tabheader__item');
+	const tabsParent = document.querySelector('.tabheader__items');
+
+	function hideTabContent() {
+		tabsContent.forEach(item => {
+			item.style.display = 'none';
+		});
+
+		tabs.forEach(item => {
+			item.classList.remove('tabheader__item_active');
+		});
+	}
+
+	function showTabContent(index = 0) {
+		tabs[index].classList.add('tabheader__item_active');
+		tabsContent[index].style.display = 'block';
+	}
+
+	tabsParent.addEventListener('click', (e) => {
+		let target = e.target;
+		if (target && target.classList.contains('tabheader__item')) {
+			tabs.forEach((item, i) => {
+				if (target === item) {
+					hideTabContent();
+					showTabContent(i);
+				}
+			});
+		}
+	});
+
+	hideTabContent();
+	showTabContent();
+
+	//Timer
+
+	const deadLine = '2021-10-26';
+
+	function getTimeRemaining(endtime) {
+		const t = Date.parse(endtime) - Date.parse(new Date());
+		const days = Math.floor(t / (1000 * 60 * 60 * 24)),
+			hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+			minutes = Math.floor((t / (1000 * 60)) % 60),
+			seconds = Math.floor((t / 1000) % 60);
+		return {
+			t,
+			days,
+			hours,
+			minutes,
+			seconds
+		};
+	}
+	
+	function setClock(selector, endtime) {
+		const timer = document.querySelector(selector);
+		const days = timer.querySelector('#days'),
+			hours = timer.querySelector('#hours'),
+			minutes = timer.querySelector('#minutes'),
+			seconds = timer.querySelector('#seconds');
+		const timeInterval = setInterval(updateClock, 1000);
+
+		updateClock();
+
+		function updateClock() {
+			const time = getTimeRemaining(endtime);
+
+			days.innerHTML = time.days < 10 ? `0${time.days}` : time.days;
+			hours.innerHTML = time.hours < 10 ? `0${time.hours}` : time.hours;
+			minutes.innerHTML = time.minutes;
+			seconds.innerHTML = time.seconds;
+
+			if (time.t <= 0) {
+				clearInterval(timeInterval);
+			}
+
+
+		}
+	}
+
+	setClock('.timer', deadLine);
+
+});
