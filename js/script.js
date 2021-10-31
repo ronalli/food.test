873b6cad1f7bde689d47df8b4e18e0361b267c72
Nikita <khakimov.nikita.vch@gmail.com>
2021-10-31 12:48:00 +0200

    slide_navigation

--- js/script.js
+++ js/script.js
@@ -293,7 +293,8 @@ window.addEventListener('DOMContentLoaded', () => {
 	let currentIndexSlider = 1;
 	let offset = 0;
 
-	const slides = document.querySelectorAll('.offer__slide');
+	const slides = document.querySelectorAll('.offer__slide'),
+		slider = document.querySelector('.offer__slider');
 	const current = document.querySelector('#current');
 	const total = document.querySelector('#total');
 	const sliderPrev = document.querySelector('.offer__slider-prev');
@@ -331,6 +332,7 @@ window.addEventListener('DOMContentLoaded', () => {
 			currentIndexSlider++;
 		}
 		indexCheck(currentIndexSlider);
+		setStyleOpacity(massDots, currentIndexSlider);
 		sliderInner.style.transform = `translateX(-${offset}px)`;
 	});
 
@@ -349,7 +351,69 @@ window.addEventListener('DOMContentLoaded', () => {
 			currentIndexSlider--;
 		}
 		indexCheck(currentIndexSlider);
+		setStyleOpacity(massDots, currentIndexSlider);
 		sliderInner.style.transform = `translateX(-${offset}px)`;
 	});
+
+
+	slider.style.position = 'relative';
+	const indicators = document.createElement('ol');
+	const massDots = [];
+	indicators.classList.add('carousel-indicators');
+	indicators.style.cssText = `
+			position: absolute;
+			right: 0;
+			bottom: 0;
+			left: 0;
+			z-index: 15;
+			display: flex;
+			justify-content: center;
+			margin-right: 15%;
+			margin-left: 15%;
+			list-style: none;
+	`;
+	slider.append(indicators);
+
+	slides.forEach((item, index) => {
+		const dot = document.createElement('li');
+		dot.setAttribute('data-slide-to', index + 1);
+		dot.style.cssText = `
+				box-sizing: content-box;
+				flex: 0 1 auto;
+				width: 30px;
+				height: 6px;
+				margin-right: 3px;
+				margin-left: 3px;
+				cursor: pointer;
+				background-color: #fff;
+				background-clip: padding-box;
+				border-top: 10px solid transparent;
+				border-bottom: 10px solid transparent;
+				opacity: .5;
+				transition: opacity .6s ease;
+		`;
+		indicators.append(dot);
+		massDots.push(dot);
+		massDots[currentIndexSlider - 1].style.opacity = '1';
+	});
+
+
+	console.log(massDots);
+
+	massDots.forEach(dot => {
+		dot.addEventListener('click', () => {
+			const index = dot.getAttribute('data-slide-to');
+			offset = (+widthSlide.slice(0, widthSlide.length - 2) * (index - 1));
+			sliderInner.style.transform = `translateX(-${offset}px)`;
+			indexCheck(index);
+			setStyleOpacity(massDots, index);
+		});
+	});
+
+
+	function setStyleOpacity(arr, index) {
+		arr.forEach(item => item.style.opacity = '0.5');
+		arr[index - 1].style.opacity = '1';
+	}
 
 });
